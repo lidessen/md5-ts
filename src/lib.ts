bf38209f5d34526c3c929af4d85b3c1885d3b1bd
Yarn <ztytotoro@users.noreply.github.com>
2019-12-23 14:36:27 +0800

    fix problem: length limit

--- src/lib.ts
+++ src/lib.ts
@@ -1,87 +1,114 @@
-export function split(target: string | any[], step: number, markString: boolean = typeof target === "string") {
-    if (typeof target === "string") target = target.split("");
+export function split(
+  target: string | any[],
+  step: number,
+  markString: boolean = typeof target === 'string'
+) {
+  if (typeof target === 'string') target = target.split('');
 
-    let result: any[] = target.map(
-        (_, index: number) =>
-            index % step === 0
-                ? Array.from(Array(step).keys()).map((x: number) => target[index + x])
-                : []
+  let result: any[] = target
+    .map((_, index: number) =>
+      index % step === 0
+        ? Array.from(Array(step).keys()).map((x: number) => target[index + x])
+        : []
     )
     .filter((x: any[]) => x.length > 0);
 
-    if (markString) result = result.map((x: any[]) => x.join(""))
+  if (markString) result = result.map((x: any[]) => x.join(''));
 
-    return result;
+  return result;
 }
 
-export function padding(str: string | any[], length: number, char: string, tail: boolean = true, isArray: boolean = Array.isArray(str)) {
-    let arr;
-    if (Array.isArray(str)) {
-        arr = str;
-    } else {
-        arr = str.split("");
-    }
-    const paddingStr: any[] = range(length - str.length).map(() => char);
-    const result = tail ? arr.concat(paddingStr) : paddingStr.concat(arr);
-    return isArray ? result : result.join("");
+export function padding(
+  str: string | any[],
+  length: number,
+  char: string,
+  tail: boolean = true,
+  isArray: boolean = Array.isArray(str)
+) {
+  let arr;
+  if (Array.isArray(str)) {
+    arr = str;
+  } else {
+    arr = str.split('');
+  }
+  const paddingStr: any[] = range(length - str.length).map(() => char);
+  const result = tail ? arr.concat(paddingStr) : paddingStr.concat(arr);
+  return isArray ? result : result.join('');
 }
 
 export function little_endian(charCode: number) {
-    return split(padding(charCode.toString(16), 8, "0", false), 2).reverse().join("");
+  return split(padding(charCode.toString(16), 8, '0', false), 2)
+    .reverse()
+    .join('');
 }
 
 export function range(...args: number[]) {
-    const start: number = args.length === 1 ? 0 : args[0];
-    const end: number = args.length === 2 ? args[1] : args[0] - 1;
+  const start: number = args.length === 1 ? 0 : args[0];
+  const end: number = args.length === 2 ? args[1] : args[0] - 1;
 
-    return Array.from(Array(end - start + 1).keys()).map((x: number) => x + start);
+  return Array.from(Array(end - start + 1).keys()).map(
+    (x: number) => x + start
+  );
 }
 
-export function to_binary(code: number, bit: number = 8, max: number = Math.pow(2, bit) - 1) {
-    if (code < 0) throw new Error("code should be greater than: 0");
-    if (code > max) throw new Error("code should be less than: " + max);
-    return padding(code.toString(2), bit, "0", false);
+export function to_binary(
+  code: number,
+  bit: number = 8,
+  max: number = Math.pow(2, bit) - 1
+) {
+  if (code < 0) throw new Error('code should be greater than: 0');
+  if (code > max) throw new Error('code should be less than: ' + max);
+  return padding(code.toString(2), bit, '0', false);
 }
 
-export function to_hex(code: number, bit: number = 8, max: number = Math.pow(16, bit) - 1) {
-    if (code < 0) throw new Error("code should be greater than: 0");
-    if (code > max) throw new Error("code should be less than: " + max);
-    return padding(code.toString(16), bit, "0", false);
+export function to_hex(
+  code: number,
+  bit: number = 8,
+  max: number = Math.pow(16, bit) - 1
+) {
+  if (code < 0) throw new Error('code should be greater than: 0');
+  if (code > max) throw new Error('code should be less than: ' + max);
+  return padding(code.toString(16), bit, '0', false);
 }
 
 export function to_code(str: string) {
-    if (str.substr(0, 2).toLowerCase() === "0b") return parseInt(str.substr(2, 8), 2);
-    if (str.substr(0, 2).toLowerCase() === "0x") return parseInt(str.substr(2, 8), 16);
+  if (str.substr(0, 2).toLowerCase() === '0b')
+    return parseInt(str.substr(2, 8), 2);
+  if (str.substr(0, 2).toLowerCase() === '0x')
+    return parseInt(str.substr(2, 8), 16);
 }
 
 export function utf16_to_utf8(str: string) {
-    return str.split("").map((char: string) => utf8_encode(char)).join("");
+  return str
+    .split('')
+    .map((char: string) => utf8_encode(char))
+    .join('');
 }
 
 export function utf8_encode(char: string) {
-    let utftext = "";
-    const c = char.charCodeAt(0);
-    if (c < 128) {
-        utftext += String.fromCharCode(c);
-    } else if ((c > 127) && (c < 2048)) {
-        utftext += String.fromCharCode((c >> 6) | 0b11000000);
-        utftext += String.fromCharCode((c & 0b00111111) | 0b10000000);
-    } else {
-        utftext += String.fromCharCode((c >> 12) | 0b11100000);
-        utftext += String.fromCharCode(((c >> 6) & 0b00111111) | 0b10000000);
-        utftext += String.fromCharCode((c & 0b00111111) | 0b10000000);
-    }
+  let utftext = '';
+  const c = char.charCodeAt(0);
+  if (c < 128) {
+    utftext += String.fromCharCode(c);
+  } else if (c > 127 && c < 2048) {
+    utftext += String.fromCharCode((c >> 6) | 0b11000000);
+    utftext += String.fromCharCode((c & 0b00111111) | 0b10000000);
+  } else {
+    utftext += String.fromCharCode((c >> 12) | 0b11100000);
+    utftext += String.fromCharCode(((c >> 6) & 0b00111111) | 0b10000000);
+    utftext += String.fromCharCode((c & 0b00111111) | 0b10000000);
+  }
 
-    return utftext;
+  return utftext;
 }
 
 export function uint_add(...args: number[]) {
-    const t = Uint32Array.from([0]);
-    const x = Uint32Array.from(args);
-    x.forEach(n => t[0] = t[0] + n);
-    return t[0];
+  const t = Uint32Array.from([0]);
+  const x = Uint32Array.from(args);
+  x.forEach(n => (t[0] = t[0] + n));
+  return t[0];
 }
 
 export function loop_shift_left(n: number, bits: number) {
-    return (n << bits) | (n >>> (32 - bits));
+  return (n << bits) | (n >>> (32 - bits));
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
-import { padding, to_binary, split, utf16_to_utf8, little_endian, uint_add, loop_shift_left } from "./lib";
+import {
+  padding,
+  to_binary,
+  split,
+  utf16_to_utf8,
+  little_endian,
+  uint_add,
+  loop_shift_left
+} from './lib';
 
 const A = 0x67452301;
 const B = 0xefcdab89;
@@ -6,96 +14,183 @@ const C = 0x98badcfe;
 const D = 0x10325476;
 
 function F(b: number, c: number, d: number) {
-    return (b & c) | (~b & d);
+  return (b & c) | (~b & d);
 }
 
 function G(b: number, c: number, d: number) {
-    return (b & d) | (c & ~d);
+  return (b & d) | (c & ~d);
 }
 
 function H(b: number, c: number, d: number) {
-    return b ^ c ^ d;
+  return b ^ c ^ d;
 }
 
 function I(b: number, c: number, d: number) {
-    return c ^ (b | ~d)
+  return c ^ (b | ~d);
 }
 
 const S = [
-    7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,
-    5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,
-    4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,
-    6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21
-]
+  7,
+  12,
+  17,
+  22,
+  7,
+  12,
+  17,
+  22,
+  7,
+  12,
+  17,
+  22,
+  7,
+  12,
+  17,
+  22,
+  5,
+  9,
+  14,
+  20,
+  5,
+  9,
+  14,
+  20,
+  5,
+  9,
+  14,
+  20,
+  5,
+  9,
+  14,
+  20,
+  4,
+  11,
+  16,
+  23,
+  4,
+  11,
+  16,
+  23,
+  4,
+  11,
+  16,
+  23,
+  4,
+  11,
+  16,
+  23,
+  6,
+  10,
+  15,
+  21,
+  6,
+  10,
+  15,
+  21,
+  6,
+  10,
+  15,
+  21,
+  6,
+  10,
+  15,
+  21
+];
 
 function T(i: number) {
-    return Math.floor(Math.pow(2, 32) * Math.abs(Math.sin(i + 1)));
+  return Math.floor(Math.pow(2, 32) * Math.abs(Math.sin(i + 1)));
 }
 
 function x_index(i: number) {
-    if (i >= 0 && i <= 15) return i;
-    if (i >= 16 && i <= 31) return (5 * i + 1) % 16;
-    if (i >= 32 && i <= 47) return (3 * i + 5) % 16;
-    if (i >= 48 && i <= 63) return (7 * i) % 16;
-    return 0;
+  if (i >= 0 && i <= 15) return i;
+  if (i >= 16 && i <= 31) return (5 * i + 1) % 16;
+  if (i >= 32 && i <= 47) return (3 * i + 5) % 16;
+  if (i >= 48 && i <= 63) return (7 * i) % 16;
+  return 0;
 }
 
 function wrap(m: any) {
-    return (a: number, b: number, c: number, d: number, x: number, s: number, t: number) => {
-        // 循环左移
-        return uint_add(loop_shift_left(uint_add(a, m(b, c, d), x, t), s), b);
-    };
+  return (
+    a: number,
+    b: number,
+    c: number,
+    d: number,
+    x: number,
+    s: number,
+    t: number
+  ) => {
+    // 循环左移
+    return uint_add(loop_shift_left(uint_add(a, m(b, c, d), x, t), s), b);
+  };
 }
 
 function porcess_message(str: string) {
-    const length = str.length;
-    const length_of_zero = Math.ceil(length / 64) * 64 - length - 8 - 1;
-    str += String.fromCharCode(0b10000000);
-    const strArray = padding(str.split(""), length + 1 + length_of_zero, String.fromCharCode(0));
+  const length = str.length;
+  const length_of_zero = Math.ceil(length / 64) * 64 - length - 8 - 1;
+  str += String.fromCharCode(0b10000000);
+  const strArray = padding(
+    str.split(''),
+    length + 1 + length_of_zero,
+    String.fromCharCode(0)
+  );
 
-    const tail = split(padding(to_binary(length * 8 % Math.pow(2, 64)), 64, "0"), 8).map(x => parseInt(x, 2));
-    const head = (strArray as any[]).map(x => x.charCodeAt(0));
-    return Uint32Array.from(
-        split(head.concat(tail), 4)
-        .map(x =>
-                x.map((t: number) => padding(t.toString(16), 2, "0", false)).join("")
-            )
-        .map(x => parseInt(x, 16))
-        .map(x => parseInt(little_endian(x), 16))
-    )
+  const tail = split(
+    padding(to_binary((length * 8) % Math.pow(2, 64), 64), 64, '0'),
+    8
+  )
+    .reverse()
+    .map(x => parseInt(x, 2));
+  const head = (strArray as any[]).map(x => x.charCodeAt(0));
+  return Uint32Array.from(
+    split(head.concat(tail), 4)
+      .map(x =>
+        x.map((t: number) => padding(t.toString(16), 2, '0', false)).join('')
+      )
+      .map(x => parseInt(x, 16))
+      .map(x => parseInt(little_endian(x), 16))
+  );
 }
 
 function fghi(i: number) {
-    if (i >= 0 && i <= 15) return F;
-    if (i >= 16 && i <= 31) return G;
-    if (i >= 32 && i <= 47) return H;
-    if (i >= 48 && i <= 63) return I;
+  if (i >= 0 && i <= 15) return F;
+  if (i >= 16 && i <= 31) return G;
+  if (i >= 32 && i <= 47) return H;
+  if (i >= 48 && i <= 63) return I;
 }
 
 function fghi_wrapped(i: number) {
-    return wrap(fghi(i));
+  return wrap(fghi(i));
 }
 
 export default function md5(str: string) {
-    str = utf16_to_utf8(str);
-    const uint32_array = porcess_message(str);
-    const result = Uint32Array.from([A, B, C, D]);
-    const chunks = split(Array.from(uint32_array), 16);
-    for (const chunk of chunks) {
-        const a = result[0];
-        const b = result[1];
-        const c = result[2];
-        const d = result[3];
+  str = utf16_to_utf8(str);
+  const uint32_array = porcess_message(str);
+  const result = Uint32Array.from([A, B, C, D]);
+  const chunks = split(Array.from(uint32_array), 16);
+  for (const chunk of chunks) {
+    const a = result[0];
+    const b = result[1];
+    const c = result[2];
+    const d = result[3];
 
-        for (let i = 0; i < 64; i++) {
-            result[(4 - i % 4) % 4] = fghi_wrapped(i)(result[(4 - i % 4) % 4], result[((4 - i % 4) + 1) % 4], result[((4 - i % 4) + 2) % 4], result[((4 - i % 4) + 3) % 4], chunk[x_index(i)], S[i], T(i))
-        }
-
-        result[0] = a + result[0];
-        result[1] = b + result[1];
-        result[2] = c + result[2];
-        result[3] = d + result[3];
-
+    for (let i = 0; i < 64; i++) {
+      result[(4 - (i % 4)) % 4] = fghi_wrapped(i)(
+        result[(4 - (i % 4)) % 4],
+        result[(4 - (i % 4) + 1) % 4],
+        result[(4 - (i % 4) + 2) % 4],
+        result[(4 - (i % 4) + 3) % 4],
+        chunk[x_index(i)],
+        S[i],
+        T(i)
+      );
     }
-    return Array.from(result).map(x => little_endian(x)).join("").toLowerCase();
+
+    result[0] = a + result[0];
+    result[1] = b + result[1];
+    result[2] = c + result[2];
+    result[3] = d + result[3];
+  }
+  return Array.from(result)
+    .map(x => little_endian(x))
+    .join('')
+    .toLowerCase();
 }
